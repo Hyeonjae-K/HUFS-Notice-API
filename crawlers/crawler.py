bd74b242c5d8e08089a7996126c74345464d0265
Hyeonjae-K <href.hjk@gmail.com>
2022-07-29 20:03:25 +0900

    FIX - 제목 생략 문제 해결 및 주석 추가

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -7,35 +7,63 @@ from notices.models import Category, Notice, Attachment
 
 def crawl_soft():
     row_selector = '#board-container > div.list > form:nth-child(2) > table > tbody > tr'
-    title_selector = 'td.title > a'
-    author_selector = 'td:nth-child(5)'
-    url_prefix = 'https://builder.hufs.ac.kr'
     content_selector = '#board-container > div.view > table > tr > td'
-    attachment_selector = 'td > a'
     category = Category.objects.get(id=1)
-    rows = [row for row in BeautifulSoup(requests.get(
-        category.url).text, 'html.parser').select(row_selector) if row.span]
-    titles = [row.select_one(title_selector).text.strip() for row in rows]
-    new_indexes = [i for i, title in enumerate(
-        titles) if not Notice.objects.filter(title=title)]
-    rows = [rows[i] for i in new_indexes]
-    titles = [titles[i] for i in new_indexes]
-    authors = [row.select_one(author_selector).text.strip() for row in rows]
-    urls = [url_prefix + '/user/' +
-            row.select_one(title_selector).get('href')for row in rows]
-    numbers = list(map(int, [url[url.rfind('=')+1:] for url in urls]))
-    detail_pages = [BeautifulSoup(requests.get(url).text, 'html.parser').select(
-        content_selector) for url in urls]
+
+    # [(author, url, numbers) or None, ...]
+    data = [_parseRow(row) for row in BeautifulSoup(requests.get(
+        category.url).text, 'html.parser').select(row_selector)]
+    # Remove None and Redundancy
+    data = [info for info in data if info and not Notice.objects.filter(
+        number=info[2])]
+
+    # info[1] = url
+    detail_pages = [BeautifulSoup(requests.get(info[1]).text, 'html.parser').select(
+        content_selector) for info in data]
+    # Remove title prefix
+    titles = [detail_page[0].text[5:] for detail_page in detail_pages]
+    # detail_page[-1] is main content section
     contents = [str(detail_page[-1].select_one('div'))
                 for detail_page in detail_pages]
-    attachments = [[(attach.text.strip(), url_prefix + attach.get('onclick')[attach.get('onclick').find("'")+1:attach.get('onclick').rfind("'")])
-                    for attach in detail_page[2].select(attachment_selector)] for detail_page in detail_pages]
-    notices = [Notice(title=title, number=number, author=author, category=category, content=content, url=url)
-               for title, number, author, content, url in zip(titles, numbers, authors, contents, urls)]
-    Notice.objects.bulk_create(notices)
+    # detail_page[2] is attachment section
+    attachments = [_parseAttachments(detail_page[2])
+                   for detail_page in detail_pages]
+
+    notices = [Notice(title=title, number=info[2], author=info[0], category=category, content=content, url=info[1])
+               for title, info, content in zip(titles, data, contents)]
+    if notices:
+        Notice.objects.bulk_create(notices)
+    # flatten 2D list to 1D list
     attachments = list(chain.from_iterable([_getAttachments(
         notice, attachment) for notice, attachment in zip(notices, attachments) if attachment]))
-    Attachment.objects.bulk_create(attachments)
+    if attachments:
+        Attachment.objects.bulk_create(attachments)
+
+
+def _parseRow(row):
+    '''if fixed notice return None
+    else (author, url, number)'''
+    title_selector = 'td.title > a'
+    author_selector = 'td:nth-child(5)'
+    url_prefix = 'https://builder.hufs.ac.kr/user/'
+
+    if not row.span:
+        return
+
+    author = row.select_one(author_selector).text.strip()
+    url = url_prefix + row.select_one(title_selector).get('href')
+    number = int(url[url.rfind('=')+1:])
+
+    return author, url, number
+
+
+def _parseAttachments(attachment_section):
+    '''return [(attachment_title, attachment_download_url), ...] or []'''
+    attachment_selector = 'td > a'
+    url_prefix = 'https://builder.hufs.ac.kr'
+    attachments = attachment_section.select(attachment_selector)
+
+    return [(attachment.text.strip(), url_prefix+attachment.get('onclick')[attachment.get('onclick').find("'")+1:attachment.get('onclick').rfind("'")]) for attachment in attachments]
 
 
 def _getAttachments(notice, attachments):
